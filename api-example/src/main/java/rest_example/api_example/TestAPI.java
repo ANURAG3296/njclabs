abdf4602fca2e9082978b7f5071d6e344d9fd457
ANURAG3296 <32014392+ANURAG3296@users.noreply.github.com>
2020-11-30 09:33:06 -0800

    updated
    updated

--- api-example/src/main/java/rest_example/api_example/TestAPI.java
+++ api-example/src/main/java/rest_example/api_example/TestAPI.java
@@ -7,9 +7,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import org.codehaus.jettison.json.JSONArray;
@@ -18,7 +21,7 @@ import org.codehaus.jettison.json.JSONObject;
 
 @Path("/userapi")
 public class TestAPI {		
-		
+	
 	    @GET 
 	    @Path("users")
 	    @Produces("application/json")
@@ -48,6 +51,7 @@ public class TestAPI {
 			return result;
 	    }
 
+	    
 	    @POST
 	    @Path("adduser")
 	    @Consumes("application/json")
@@ -68,6 +72,48 @@ public class TestAPI {
 	    	}
 	    	return "Success";
 	    }
+	    
+	    
+	    @DELETE
+	    @Path("deleteuser/{id}")
+	    @Consumes("application/json")
+	    public String deleteUser ( @PathParam("id") int id) throws JSONException, ClassNotFoundException{
+			
+	    	try {
+	    		Connection conn = this.getConnection();
+	    		int uid = id;
+	    		String sql = "DELETE FROM USERS WHERE userid =" + uid ;
+	    		Statement st = conn.createStatement();
+	    		st.executeUpdate(sql);
+	    	}
+	    	catch(SQLException e) {
+	    		return "Failure";
+	    	}
+	    	return "Success";	
+	    }
+
+	    
+	    @PUT
+	    @Path("updateuser")
+	    @Consumes("application/json")
+	    public String updateuser (JSONObject obj) throws JSONException, ClassNotFoundException{
+	    	try {		
+	    		Connection conn = this.getConnection();
+	    		int id = obj.getInt("id");
+		    	String first_name = obj.getString("first_name");
+		    	String last_name = obj.getString("last_name");
+		    	String city = obj.getString("city");
+		    	
+				String sql = "UPDATE USERS SET userid = '"+id+"',firstname = '"+first_name+"',lastname = '"+last_name+"', city='"+city+"' WHERE userid = "+ id;     
+				Statement st = conn.createStatement();
+				st.executeUpdate(sql);
+	    	}
+	    	catch(SQLException e) {
+	    		return "Failure";
+	   		}
+	   	return "Success";
+	   }
+    
 	    
 	    private Connection getConnection() throws ClassNotFoundException, SQLException {
 			String url = "jdbc:mysql://localhost:3306/resttest";
